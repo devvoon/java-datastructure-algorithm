8270633f1a3354b3b46bde25cb835b093670bdda
dawoonjeong <iamdawoonjeong@gmail.com>
2020-04-28 21:03:09 +0900

    add recursion-sum
    add recursion-sum

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# java-algorithm
- 
+# JAVA Algorithm (자바 알고리즘)
+
+
+----------
+
+
+## 1. java-algorithm-theory
+알고리즘의 기본 이론내용을 JAVA로 풀이  
+
+
+### 1.  Recursion (순환)  
+1.  Sum.java : 1 ~ n 까지의 합 구하기 (Recrusion, for)
---
+++ java-algorithm-theory/src/recursion/Sum.java
@@ -0,0 +1,61 @@
+package recursion;
+
+public class Sum {
+
+	/**
+	 * resuion - 1~~n까지의 sum을 구하기
+	 * @param args
+	 */
+    public static void main(String[] args) {
+
+        int n = 5;
+        System.out.println("n : " + n);
+        
+        /* 1~ n 까지의 합 구하기 */
+        // recursion
+        System.out.println("=====recusrion을 이용한 1~n까지의 합 구하기=====");
+        int sumOfRecursion = functionRecursion(n);
+        System.out.println( );
+        System.out.println("sum of 1 ~ n  : " + sumOfRecursion);
+
+        //for
+        System.out.println("=====for을 이용한 1~n까지의 합 구하기=====");
+        int sumOfFor = functionFor(n);
+        System.out.println( );
+        System.out.println("sum of 1 ~ n  : " + sumOfFor);        
+    }
+    
+    /**
+     * recusrion을 이용한 1~n까지의 합 구하기
+     * @param n
+     * @return
+     */
+    public static int functionRecursion(int n) {
+        if(n==0) {
+            return 0;
+        }else {
+            System.out.print(n + " ");
+            return n + functionRecursion(n-1);
+        }
+    }
+    
+    /**
+     * for을 이용한 1~n까지의 합 구하기
+     * @param n
+     * @return
+     */
+    public static int functionFor(int n) {
+        int sum = 0;
+        
+        //1~n 까지의 합 
+        // for (int i = 1; i <= n; i++) {
+        
+        //n~1 까지의 합 
+        for (int i = n ;  i > 0 ; i--) {       
+            sum = sum + i;
+            System.out.print(sum + " ");
+        }
+        return sum;
+    }
+    
+}
