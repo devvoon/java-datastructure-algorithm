b7365b872205a67ec8b822cfacb180b8d332a3e2
dawoonjeong <iamdawoonjeong@gmail.com>
2020-06-30 18:40:38 +0900

    Create Fibonacci.java
    Create Fibonacci.java

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # JAVA Algorithm (자바 알고리즘)
-
-
-----------
-
-
-## 1. java-algorithm-theory
 알고리즘의 기본 이론내용을 JAVA로 풀이  
+- Recursion (순환) 
 
-
-### 1.  Recursion (순환)  
-1.  Sum : 1 ~ n 까지의 합 구하기 (Recrusion, for)
-2.  Factorial : n! 구하기 (Recrusion, for)
+## Recursion (순환)  
+### 1.  Sum : 1 ~ n 까지의 합 구하기 (Recrusion, for)
+### 2.  Factorial : n! 구하기 (Recrusion, for)
+### 3.  Fibonacci 구하기 (Recrusion, for)
---
+++ java-algorithm-theory/src/recursion/Fibonacci.java
@@ -0,0 +1,65 @@
+package recursion;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+ 
+        int n = 10;
+        System.out.println("n : "  + n);
+        
+        /* fibonacci 구하기 */
+        //recursion
+        System.out.println("=====recusrion을 fibonacci수 구하기=====");
+        
+        for (int i = 0; i < n; i++) {
+            int fibonacciOfRecusrion = fibonacciRecusrion(i);
+            System.out.print(fibonacciOfRecusrion + " ");
+        }
+        System.out.println( );
+        
+        // for
+        System.out.println("=====recusrion을 fibonacci수 구하기=====");
+        for (int i = 0; i < n; i++) {
+            int fibonacciOfFor = fibonacciFor(i);
+            System.out.print(fibonacciOfFor + " ");
+        }
+
+    }
+    
+    /**
+     *  recusrion을 fibonacci수 구하기
+     * @param n
+     * @return
+     */
+    public static int fibonacciRecusrion(int n) {
+        if(n < 2) {
+            return n;
+        }else{
+            return fibonacciRecusrion(n-1) + fibonacciRecusrion(n-2);
+        }
+    }
+    
+    /**
+     * recusrion을 fibonacci수 구하기
+     * @param n
+     * @return
+     */
+    public static int fibonacciFor(int n) {
+          if (n < 2) {
+              return n;
+          } else {
+              
+              int a = 0; //f0
+              int b = 1;  //f1 
+              int sum = 0;   //fn 
+              
+              for (int i = 2; i <= n; i++) {
+                  sum = a + b; // fn = f(n-1) + f(n-2)  = f0 + f1
+                  a = b;
+                  b = sum;
+              }
+              return sum;
+          }
+    }
+
+}
