4991355e764b820c426df18edfaa37447d56e63a
dawoonjeong <iamdawoonjeong@gmail.com>
2020-07-06 21:41:51 +0900

    Create datastructure-LinkedList (arrayList, linkedList)
    Create datastructure-LinkedList (arrayList, linkedList)

---
+++ java-datastructure/src/list/ArrayListExample.java
@@ -0,0 +1,68 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * ArrayList Collection Framework를 이용한 ArrayList 사용해보기 
+ * @author iamdawoonjeong
+ *
+ */
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		
+		
+        // Creating object of the class Array List
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(40);
+		
+		for (int i = 0 ; numbers.size() > i ; i++) {
+			System.out.print( i + "," + numbers.get(i)  + " ");
+		}
+
+		System.out.println();
+		System.out.println("===== insertion : add(1,50) =====");
+		//인덱스 1에 값 50을 넣고, 기존 값들은 뒤로 밀림
+		numbers.add(1, 50);
+		
+		for (int i = 0 ; numbers.size() > i ; i++) {
+			System.out.print( i + "," + numbers.get(i)  + " ");
+		}
+		
+		
+		System.out.println();
+		System.out.println("===== 순차적으로 읽기  - Iterator =====");
+		
+		Iterator<Integer> intIterator = numbers.iterator();
+		while (intIterator.hasNext()) {
+			int value = intIterator.next();
+			System.out.print(value + " " );
+			
+		}
+		
+		System.out.println();
+		System.out.println("===== 순차적으로 읽기 - foreach =====");
+		
+		for (Integer intForeach : numbers) {
+			System.out.print(intForeach + " ");
+		}
+		
+
+		System.out.println();
+		System.out.println("=====  remove  =====");
+		//인덱스 2에 remove 하면 뒤에 값들이 앞으로 당겨짐(LIST의 특징인 빈공간이 없는 장점) 
+		numbers.remove(2);
+		for (int i = 0 ; numbers.size() > i ; i++) {
+			System.out.print( i + "," + numbers.get(i)  + " ");
+		}
+		
+		
+		
+	}
+
+}
---
+++ java-datastructure/src/list/LinkedListExample.java
@@ -0,0 +1,75 @@
+package list;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * LinkedList Collection Framework를 이용한 LinkedList 사용해보기
+ * @author iamdawoonjeong
+ *
+ */
+public class LinkedListExample {
+	
+	public static void main(String[] args) {
+
+        // Creating object of the class linked list 
+        LinkedList<String> ll = new LinkedList<String>(); 
+  
+        // Adding elements to the linked list 
+        System.out.println("===== insertion : linkedList.add(element) =====");
+        ll.add("A"); 
+        ll.add("B"); 
+        System.out.println(ll);
+        
+        System.out.println("===== insertion : addLast(element), addFrist(element) =====");
+        ll.addLast("C"); 
+        System.out.println(ll);
+        
+        ll.addFirst("D"); 
+        System.out.println(ll);
+        
+        System.out.println("===== store : add(index, element) =====");
+        ll.add(2, "E"); 
+        System.out.println(ll); 
+  
+        System.out.println("===== deletion : remove(element), remove(index) =====");
+        ll.remove("B");
+        System.out.println(ll); 
+        
+        ll.remove(3); 
+        System.out.println(ll); 
+        
+        System.out.println("===== deletion : reremoveFirstmove(), removeLast() =====");
+        ll.removeFirst(); 
+        ll.removeLast(); 
+        System.out.println(ll); 
+        
+        System.out.println("===== insertion ===== ");
+        ll.add("B"); 
+        ll.add("C"); 
+        ll.add("D");
+        ll.add("E"); 
+        System.out.println(ll);
+        
+        System.out.println("===== length : size() =====");
+        System.out.println(ll.size());
+        
+        System.out.println("===== searching : get(index) =====");
+        System.out.println(ll.get(0)); 
+        
+        System.out.println("===== 순차적으로 읽기 - for =====");
+        for (int i = 0; i < ll.size(); i++) {
+        	System.out.print(ll.get(i) + " ");
+		}
+        
+        System.out.println();
+        System.out.println("===== 순차적으로 읽기 - Iterator =====");
+        Iterator<String> it = ll.iterator();
+        while(it.hasNext()) {
+        	String values = it.next();
+        	System.out.println(values + " ");
+        }
+
+	}
+
+}
