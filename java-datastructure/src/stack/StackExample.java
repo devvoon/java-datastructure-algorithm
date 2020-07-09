c9d518c02f54faa4ac1eff2cef09147323e93872
dawoonjeong <iamdawoonjeong@gmail.com>
2020-07-09 20:53:54 +0900

    Create Stack
    Create Stack

---
+++ java-datastructure/src/stack/StackExample.java
@@ -0,0 +1,53 @@
+package stack;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * Stack Framework Collection을 이용하여 Stack 사용해보기
+ * @author iamdawoonjeong
+ *
+ */
+public class StackExample {
+
+	public static void main(String[] args) {
+		
+		//Stack class 사용 
+		
+		System.out.println("===== stack 생성 =====");
+		Stack<Integer> stk = new Stack<Integer>();
+		
+		System.out.println("===== insertion :  저장 add() =====");
+		stk.add(1);
+		stk.add(2);
+		stk.add(3);
+		stk.add(4);
+		stk.add(5);
+		
+		System.out.println(stk.peek());
+		
+		System.out.println("=====  deletion  : 삭제 remove() =====");
+		stk.remove(4);
+		stk.remove(3);
+		
+		System.out.println(stk);
+		
+		System.out.println("=====  peek() =====");
+		System.out.println(stk.peek());
+		
+		
+		System.out.println("===== access : 순차적으로 읽기 - Iterator =====");
+		Iterator<Integer> iterator = stk.iterator();  
+		while(iterator.hasNext())  
+		{  
+			int values = iterator.next();  
+			System.out.print(values + " ") ;   
+		}     
+		
+		
+	}
+	
+	
+
+
+}
---
+++ java-datastructure/src/stack/implementation/StackMain.java
@@ -0,0 +1,59 @@
+package stack.implementation;
+
+public class StackMain {
+
+	/**
+	 * stack implementation 실행 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("===== createion stack and check size of stack=====");
+		//create()
+		Stack stack = new Stack(5);
+		System.out.println(stack.size());
+		
+		System.out.println("===== insertion  : 저장 push() =====");
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		stack.push(40);
+		stack.push(50);
+		
+		System.out.println();
+		System.out.println("===== access =====");
+		System.out.println(stack);
+		
+		
+		System.out.println();
+		System.out.println("===== insertion (more than stack size) : overflow occur =====");
+		stack.push(60);
+		
+		
+		System.out.println();
+		System.out.println("===== deletion : pop() =====");
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		
+		System.out.println();
+		System.out.println("===== search : top() =====");
+		System.out.println(stack.top());
+		
+		System.out.println();
+		System.out.println("===== access =====");
+		System.out.println(stack);
+		
+		
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		
+		System.out.println("===== deletion (empty stack)  =====");
+		System.out.println(stack.pop());
+		System.out.println(stack.top());
+		System.out.println(stack);		
+		
+		
+	}
+
+}
