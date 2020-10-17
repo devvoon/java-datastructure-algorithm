b74def6c6f35a6ebaface76e00409eb2048246e1
dawoonjeong <iamdawoonjeong@gmail.com>
2020-10-17 22:30:00 +0900

    update SortAPI.java

--- java-basic/src/sort/SortAPI.java
+++ java-basic/src/sort/SortAPI.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
+import com.sun.source.tree.NewClassTree;
+
 public class SortAPI {
 
     public static void main(String[] args) {
@@ -64,6 +66,51 @@ public class SortAPI {
 
         for (String string : arr2) {
             System.out.println(string);
+        }
+        
+        // arrays- 2차원 정렬
+        System.out.println("** Sorting Integer two dimensional array in ascending order: Arrays.sort()");
+        int[][] two = new int[5][2];
+        two[0][0] = 1;
+        two[1][0] = 2;
+        two[2][0] = 3;
+        two[3][0] = 1;
+        two[4][0] = 5;
+    
+        two[0][1] = 5;
+        two[1][1] = 4;
+        two[2][1] = 3;
+        two[3][1] = 2;
+        two[4][1] = 1;
+        
+        Arrays.sort(two, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]-o2[0];
+            }
+        });
+        
+        for (int[] is : two) {
+            System.out.println(is[0] + " " + is[1]);
+        }
+    
+        // arrays- 2차원 정렬 2번째 인덱스도 정렬 
+        System.out.println("** Sorting second index Integer two dimensional array in ascending order: Arrays.sort()");
+        Arrays.sort(two, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return Integer.compare(o1[1], o2[1]);
+                }else {
+                    return Integer.compare(o1[0], o2[0]);
+                }
+            }
+        });
+        
+        for (int[] is : two) {
+            System.out.println(is[0] + " " + is[1]);
         }
         
         // collections - 숫자 오름차순
