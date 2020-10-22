bbf0823e50f26733f9b59f5198a0f5282ea2d23c
dawoonjeong <iamdawoonjeong@gmail.com>
2020-10-22 21:11:30 +0900

    update file name

--- java-algorithm-problem-solving/src/baekjoon/kthnumber11004/Main.java
+++ java-algorithm-problem-solving/src/baekjoon/kthnumber11004/Main.java
@@ -3,38 +3,66 @@ package baekjoon.kthnumber11004;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         
-        try {
-            String[] str = br.readLine().split(" ");
-            int n = Integer.parseInt(str[0]);
-            int k = Integer.parseInt(str[1]);
-            
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            LinkedList<Integer> list = new LinkedList<Integer>();
-            
-            for (int i = 0; i < n; i++) {
-                list.add(Integer.parseInt(st.nextToken())); 
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        br.close();
+        isr.close();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+             arr[i] = (Integer.parseInt(st.nextToken())); 
+        }
+        
+        int[] result = sort(arr, 0, arr.length-1);
+        System.out.println(result[k-1]);
+    }
+
+    private static int[] sort(int[] arr, int start, int end) {
+        int left = start;
+        int right = end;
+        
+        int pivot = arr[(left+right)/2];
+        
+        while(left <= right) {
+            while( arr[left] < pivot) {
+                left++;
             }
             
-            Collections.sort(list);
-            System.out.println(list.get(k-1));
+            while (arr[right] > pivot) {
+                right--;
+            }
             
-            br.close();
-            isr.close();
-                    
-        } catch (IOException e) {
-            e.printStackTrace();
+            if (left <= right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                
+                left++;
+                right--;
+            }
         }
-
+        
+        if(start < right) {
+            sort (arr, start, right);
+        }
+        
+        if (left < end) {
+            sort(arr, left, end);
+        }
+        
+        return arr;
     }
 
 }
---
+++ java-algorithm-problem-solving/src/baekjoon/kthnumber11004/MainCollection.java
@@ -0,0 +1,40 @@
+package baekjoon.kthnumber11004;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class MainCollection {
+
+    public static void main(String[] args) {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        
+        try {
+            String[] str = br.readLine().split(" ");
+            int n = Integer.parseInt(str[0]);
+            int k = Integer.parseInt(str[1]);
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            LinkedList<Integer> list = new LinkedList<Integer>();
+            
+            for (int i = 0; i < n; i++) {
+                list.add(Integer.parseInt(st.nextToken())); 
+            }
+            
+            Collections.sort(list);
+            System.out.println(list.get(k-1));
+            
+            br.close();
+            isr.close();
+                    
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
