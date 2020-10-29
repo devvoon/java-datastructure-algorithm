37b76fdff00bf0d021720a8bca2de987b4c499e8
dawoonjeong <iamdawoonjeong@gmail.com>
2020-10-29 21:52:00 +0900

    update comments

--- java-algorithm-problem-solving/src/baekjoon/problem1991/Main.java
+++ java-algorithm-problem-solving/src/baekjoon/problem1991/Main.java
@@ -17,15 +17,19 @@ public class Main {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         
-        int n = Integer.parseInt(br.readLine());
-        Tree tree = new Tree();
+        int n = Integer.parseInt(br.readLine()); //노드 갯수 
+        
+        // 트리생성
+        Tree tree = new Tree(); 
 
+        // 각 node에  값 넣어주기
         char ch[];
         for (int i = 0; i < n; i++) {
-            ch = br.readLine().replaceAll(" ", "").toCharArray();
-            tree.add(ch[0], ch[1], ch[2]);
+            ch = br.readLine().replaceAll(" ", "").toCharArray(); //value 나누기 
+            tree.add(ch[0], ch[1], ch[2]); // 입력받은 value를 tree에 add (root, left, right) 
         } 
         
+        // 순회하기 
         tree.preorder(tree.root);
         System.out.println();
         tree.inorder(tree.root);
@@ -38,6 +42,10 @@ public class Main {
 
 }
 
+/**
+ * 노드클래스
+ *
+ */
 class Node{
 
     public char data;
@@ -51,11 +59,14 @@ class Node{
 
 class Tree{
     Node root;
-    public void add(char input, char left, char right) {
+    public void add(char data, char left, char right) {
         
+        // root가 비었을 경우
         if (null == root) {
-            if('.' != input ) {
-                root = new Node(input);
+            
+            // 자식이 없는 경우를 제외하고 각 노드에 값 넣어주어 root 만들기 
+            if('.' != data ) {
+                root = new Node(data);
             }
             if ('.' != left) {
                 root.left = new Node(left);
@@ -64,14 +75,20 @@ class Tree{
                 root.right = new Node(right);
             }
         }else {
-            search(root, input, left, right);
+            
+            // root 가 있을 경우 
+            search(root, data, left, right);
         }
     }
     
-    public void search(Node root, char input, char left, char right) {
+    public void search(Node root, char data, char left, char right) {
+        
+        // root 가 비었을 경우 
         if(null == root) {
             return;
-        }else if(root.data == input) {
+        }else if(root.data == data) {
+            
+            //입력된 데이터가 부모데이터일 경우 자식노드 값 넣어주기
             if ('.' != left) {
                 root.left = new Node(left);
             }
@@ -79,12 +96,18 @@ class Tree{
                 root.right = new Node(right);
             }
         }else {
-            search(root.left, input, left, right);
-            search(root.right, input, left, right);
+            
+            // 입력된 데이터가 root가 아닌 자식 노드에 있을 경우 재귀함수를 이용하여 부모위치 찾기
+            search(root.left, data, left, right);
+            search(root.right, data, left, right);
         }
         
     }
     
+    /**
+     * 전위 순회
+     * @param root
+     */
     public void preorder(Node root) {
         
         System.out.print(root.data);
@@ -98,6 +121,10 @@ class Tree{
         }
     }
     
+    /**
+     * 중위 순회
+     * @param root
+     */
     public void inorder(Node root) {
         
         if (null != root.left) {
@@ -111,6 +138,10 @@ class Tree{
         }
     }
     
+    /**
+     * 후위 순회
+     * @param root
+     */
     public void postorder(Node root) {
         
         if (null != root.left) {
