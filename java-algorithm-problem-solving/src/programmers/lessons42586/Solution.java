d0e9d89967c7f637243fe210959fd8d109d7ffdb
dawoonjeong <iamdawoonjeong@gmail.com>
2020-10-30 23:30:03 +0900

    Create java probolem solving programmers lessons 42586

---
+++ java-algorithm-problem-solving/src/programmers/lessons42586/Solution.java
@@ -0,0 +1,52 @@
+package programmers.lessons42586;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * 42587 프린터 - priority queue 사용 
+ * 
+ * @reference https://programmers.co.kr/learn/courses/30/lessons/42587
+ * @author iamdawoonjeong
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+        int[] input = new int[]{7,1,5,6};//{1,1,9,1,1,1}; //{2, 1, 3, 2}; // 
+        int location =2 ; // 2; //
+        
+        int answer = solution(input, location);
+        System.out.println(answer);
+
+    }
+    
+    public static int solution(int[] priorities, int location) {
+        int answer = 0; //출력횟수
+        
+        //queue에 대기문서 목록을 넣어 줌 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (Integer priority : priorities) {
+            //[3, 2, 2, 1] 이렇게 됨 
+            pq.offer(priority);
+        }
+   
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (pq.peek() == priorities[i]) {
+                    pq.poll();
+                    answer+=1;
+                    
+                    if (location == i) {
+                        pq.clear();
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+
+}
---
+++ java-algorithm-problem-solving/src/programmers/lessons42586/Solution2.java
@@ -0,0 +1,68 @@
+package programmers.lessons42586;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 42587 프린터 - queue 사용
+ * 
+ * @reference https://programmers.co.kr/learn/courses/30/lessons/42587
+ * @author iamdawoonjeong
+ */
+public class Solution2 {
+
+    public static void main(String[] args) {
+        int[] input = new int[]{2, 1, 3, 2}; // {7,1,5,6};//{1,1,9,1,1,1}; //
+        int location = 2 ; // 2; //0; //
+        
+        int answer = solution(input, location);
+        System.out.println(answer);
+    }
+    
+    public static int solution(int[] priorities, int location) {
+        int answer = 0; //출력횟수
+        
+        //queue에 대기문서 목록을 넣어 줌 
+        Queue<Integer> queue = new LinkedList<Integer>();
+        
+        for (Integer priority : priorities) {
+            queue.add(priority);
+        }
+        
+        Arrays.sort(priorities);
+   
+        int index = location;
+        int maxIndex = priorities.length-1;
+        
+        while (!queue.isEmpty()) {
+            
+            // 1. 출력되는 경우 
+            if ( queue.peek() == priorities[maxIndex-answer] ) {
+                queue.poll();
+                answer+=1;
+                
+                if (index == 0) {
+                    queue.clear();
+                    break;
+                }else {
+                    index--;
+                }
+                
+            }else {
+             // 2. 출력되지 않는 경우
+               queue.add(queue.poll());
+               
+               // 내가찾아야하는 문서가 다시 맨뒤로 가야하면, index값 재조정
+               if ( index == 0) {
+                   //맨뒤로 가야하니 현 queue사이즈에서 -1 
+                   index = queue.size()-1;
+               }else {
+                   index--;
+               }
+            }
+        }
+        
+        return answer;
+    }
+}
