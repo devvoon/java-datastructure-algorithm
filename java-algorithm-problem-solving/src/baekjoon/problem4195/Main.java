a88df5505285449cbb65b2c3515f932a5e1aba32
dawoonjeong <iamdawoonjeong@gmail.com>
2021-01-16 13:09:11 +0900

    Create java-algorithm-problem-solving\bin\baekjoon\problem4195\Main.

--- java-algorithm-problem-solving/src/baekjoon/problem4195/Main.java
+++ java-algorithm-problem-solving/src/baekjoon/problem4195/Main.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -18,86 +17,75 @@ import java.util.StringTokenizer;
  */
 public class Main {
     
-    static Map<String, Integer> LIST ;
-    static int[] PARENT, RANK, RELATION;
-    static int MAX = 100001;
+    static Map<String, Integer> List;
+    static int[] Parent, Number;
+    static int MAX = 200002;
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        
         int testCase = Integer.parseInt(br.readLine());
         
         for (int i = 0; i < testCase; i++) {
             int F = Integer.parseInt(br.readLine());
-            int index = 0;
             
-            LIST = new HashMap<>();
-            PARENT = new int[MAX];
-            RANK = new int[MAX];
-            RELATION = new int[MAX];
+            List = new HashMap<String, Integer>();
+            Parent = new int[MAX];
+            Number = new int[MAX];
             
-            Arrays.fill(RELATION, 1);
+            int index =0;
             
             for (int j = 0; j < F; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String name1 = st.nextToken();
                 String name2 = st.nextToken();
                 
-                if (!LIST.containsKey(name1)) {
-                    LIST.put(name1, index);
-                    PARENT[index] = index++;
+                if (!List.containsKey(name1)) {
+                    List.put(name1, index);
+                    Parent[index] = index;
+                    Number[index] = 1;
+                    index++;
                 }
                 
-                if(!LIST.containsKey(name2)) {
-                    LIST.put(name2, index);
-                    PARENT[index] = index++;
+                if (!List.containsKey(name2)) {
+                    List.put(name2, index);
+                    Parent[index] = index;
+                    Number[index] = 1;
+                    index++;
                 }
                 
-                union(LIST.get(name1), LIST.get(name2));
-                
-                bw.write(RELATION[find(LIST.get(name1))] + "\n");
+                unioun(List.get(name1), List.get(name2));
+                bw.write(Number[find(List.get(name1))]+"\n");
             }
         }
-
+        
+        
+        
         bw.flush();
         bw.close();
-        
         br.close();
     }
 
-
-    public static void union(int u, int v) {
-        int uR = find(u);
-        int vR = find(v);
+    public static void unioun(int x, int y) {
+        int xR = find(x);
+        int yR = find(y);
         
-        if (uR == vR) {
-            return;
-        }
-        
-        if(RANK[uR] > RANK[vR]) {
-            swap(uR, vR);
-        }
-        
-        PARENT[uR] = vR;
-        RELATION[vR] += RELATION[uR];
-        
-        if(RANK[uR] == RANK[vR]) {
-            RANK[vR]++;
+        if( xR != yR) {
+            Parent[yR] = xR;
+            Number[xR] += Number[yR];
         }
     }
 
-    public static int find(int u) {
-        if (PARENT[u] == u ) {
-            return u;
+    public static int find(int x) {
+        if ( x== Parent[x]) {
+            return x;
+        }else {
+            int p = find(Parent[x]);
+            Parent[x] = p;
+            return Parent[x];
         }
-        return (PARENT[u] = find(PARENT[u]));
-    }
     
-    public static void swap(int n1, int n2) {
-        int temp = n1;
-        n1 = n2;
-        n2 = temp;
     }
 
    
