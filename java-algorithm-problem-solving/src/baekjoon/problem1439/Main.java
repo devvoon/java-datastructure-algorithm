63df67d0521c1ad6e00ca4b13d080f4558547154
dawoonjeong <iamdawoonjeong@gmail.com>
2021-04-01 19:02:40 +0900

    Update java-algorithm-problem-solving\src\baekjoon\problem1439\Main.java

--- java-algorithm-problem-solving/src/baekjoon/problem1439/Main.java
+++ java-algorithm-problem-solving/src/baekjoon/problem1439/Main.java
@@ -25,17 +25,19 @@ public class Main {
         int count1 = 0;
         
         if (arr[0] == 1) {
-            count0 += 1;
+            count0 += 1; //첫 숫자가 1인경우, 모두 0으로 만들기 위한 횟수 추가 
         }else {
-            count1 += 1;
+            count1 += 1; //첫 숫자가 0인경우, 모두 1로 만들기 위한 횟수 추가 
         }
         
         for (int i = 0; i < arr.length-1; i++) {
 
             if (arr[i] != arr[i+1]) {
                 if(arr[i+1] == 1 ) {
+                    //다음 수가 1인 경우 
                     count0 += 1;
                 }else {
+                    //다음수가 0인 경우 
                     count1 += 1;
                 }
             }
