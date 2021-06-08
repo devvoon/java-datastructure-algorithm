63b91311e25930f7e462ce96cf45e48b7ebffdbb
dawoonjeong <iamdawoonjeong@gmail.com>
2021-06-08 12:49:05 +0900

    update comment

--- java-algorithm-problem-solving/src/programmers/heap/Lessons42626.java
+++ java-algorithm-problem-solving/src/programmers/heap/Lessons42626.java
@@ -2,6 +2,12 @@ package programmers.heap;
 
 import java.util.PriorityQueue;
 
+/**
+ * 42626 더 맵게
+ * 
+ * @reference https://programmers.co.kr/learn/courses/30/lessons/42626
+ * @author iamdawoonjeong
+ */
 public class Lessons42626 {
     public static void main(String[] args) {
 
